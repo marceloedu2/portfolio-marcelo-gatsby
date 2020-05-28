9234dd5d668c2ab2f8d8866fc9f8aea50725dd89
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-05-28 14:57:18 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import loadable from '@loadable/component'
 export default function Home() {
   const Introduction = loadable(() => import('../components/Introduction'))
   const NavBar = loadable(() => import('../components/NavBar'))
-  const About = loadable(() => import('../components/About'))
+  // const About = loadable(() => import('../components/About'))
   return (
     <>
       <GlobalStyle />
       <NavBar />
       <Introduction />
-      <About />
+      {/*<About />*/}
     </>
   )
 }
