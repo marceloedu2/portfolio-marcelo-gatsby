ccf72885349c8e959f0e3dd65fbe344ab477c550
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-05-28 15:11:14 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from "react"
 import GlobalStyle from "../styles/global"
 import loadable from '@loadable/component'
-// import Introduction from "../components/Introduction"
-// import NavBar from "../components/NavBar"
-// import About from "../components/about"
 
 export default function Home() {
+  
   const Introduction = loadable(() => import('../components/Introduction'))
   const NavBar = loadable(() => import('../components/NavBar'))
-  // const About = loadable(() => import('../components/About'))
+  const About = loadable(() => import('../components/About'))
+
   return (
     <>
       <GlobalStyle />
       <NavBar />
       <Introduction />
-      {/*<About />*/}
+      <About />
     </>
   )
 }
