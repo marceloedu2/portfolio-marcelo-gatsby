c38b079bcc215bde0ad0242fc43e0f8bcb7d6630
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-05-29 09:52:45 -0300

    teste

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
 const path = require("path")
 
 module.exports = {
   siteMetadata: {
-    title: `Portfolio - Marcelo Araujo - Densenvolvedor de sistemas`,
+    title: `Portfolio - Marcelo Araujo`,
     siteUrl: `https://www.marceloaraujodev.online`,
     description: `Desenvolvedor Front-end, Back-end e Mobile.`,
   },
