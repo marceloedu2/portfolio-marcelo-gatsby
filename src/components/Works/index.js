a378ba9770b881611adf90d4221cb98aef568dfc
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-06-01 21:52:17 -0300

    adicionando umltimos trabalhos

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -1,8 +1,41 @@
 import React from "react"
-import { Container } from "./styles"
+import {
+  Card,
+  CardContent,
+  Container,
+  Content,
+  Description,
+  Link,
+  Title,
+} from "./styles"
 
 const Works = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <Title>Ultimos Trabalhos</Title>
+      <Content>
+        <Card image="https://i.ytimg.com/vi/TZh5L9rFTno/maxresdefault.jpg">
+          <CardContent>
+            <Description>Gr8 Sistemas,Sistema de Gestão escolar</Description>
+            <Link
+              href="https://www.gr8.com.br/portal/default.asp"
+              target="_blank"
+            >
+              Conferir
+            </Link>
+          </CardContent>
+        </Card>
+        <Card image="https://media-exp1.licdn.com/dms/image/C4D1BAQEetN7_2fxj7A/company-background_10000/0?e=2159024400&v=beta&t=VSwGXVbW-PgITxV9Yg78oQhm8OnNRw5hmKW0FCAESvE">
+          <CardContent>
+            <Description>emasters, Torneios para todos</Description>
+            <Link href="https://www.emastersapp.com/" target="_blank">
+              Conferir
+            </Link>
+          </CardContent>
+        </Card>
+      </Content>
+    </Container>
+  )
 }
 
 export default Works
--- src/components/Works/styles.js
+++ src/components/Works/styles.js
@@ -2,12 +2,11 @@ import styled, { css, down } from "@xstyled/styled-components"
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: row;
-  text-align: center;
-  flex-flow: row wrap;
+  flex-direction: column;
+  text-align: start;
   width: 100%;
   padding: 0 20px;
-  height: 750px;
+  height: 550px;
   background-color: ${({ theme }) => theme.white};
   ${down(
     "md",
@@ -15,4 +14,90 @@ export const Container = styled.div`
       height: auto;
     `
   )}
+`
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  font-size: 6;
+  font-weight: bold;
+  text-align: center;
+  height: 60px;
+  margin-bottom: 5;
+  color: ${props => props.theme.darkBlue};
+  &:after {
+    content: " ";
+    width: 60px;
+    margin-top: 2;
+    border-bottom: 3.5px solid ${({ theme }) => theme.darkBlue};
+  }
+`
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex-flow: row wrap;
+  margin-bottom: 50px;
+`
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${props => props.image});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 350px;
+  height: 250px;
+  border-radius: 10px;
+  margin: 3;
+`
+export const Description = styled.div`
+  display: none;
+`
+export const Link = styled.a`
+  display: none;
+`
+
+export const CardContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background-color: rgba(84, 0, 193, 0.2);
+  &:hover {
+    background-color: rgba(84, 0, 193, 0.5);
+    ${Description} {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 20px;
+      font-size: 4;
+      font-weight: 400;
+      width: 200px;
+      color: ${({ theme }) => theme.white};
+    }
+    ${Link} {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 150px;
+      height: 35px;
+      border-radius: 30px;
+      font-size: 3;
+      font-weight: 500;
+      cursor: pointer;
+      color: ${({ theme }) => theme.white};
+      background-color: ${({ theme }) => theme.purple};
+    }
+  }
 `
